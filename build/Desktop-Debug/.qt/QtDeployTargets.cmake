efc059bf5bc66a65ccfc933cd900c63b1c4f19c7
ivanos121 <loggie121@gmail.com>
2026-06-24 12:38:37 +0300

    fix CMakeFileLists.txt and .gitignore

--- build/Desktop-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_pytorch-studio_FILE /home/elf/pyTorch-Studio/build/Desktop-Debug/pytorch-studio)
-set(__QT_DEPLOY_TARGET_pytorch-studio_TYPE EXECUTABLE)
--- build/Desktop-Debug/CMakeFiles/pytorch-studio.dir/pytorch-studio_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/pytorch-studio.dir/pytorch-studio_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/elf/pyTorch-Studio/build/Desktop-Debug/pytorch-studio_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
